7ddb041ddc1766ef89b4a945c338c76d146c032b
SamGDuarte <126623537+SamGDuarte@users.noreply.github.com>
2024-05-22 20:27:13 -0300

    Update TesteSuite.java

--- calculadora/src/test/java/TesteSuite.java
+++ calculadora/src/test/java/TesteSuite.java
@@ -1,19 +1,15 @@
-
 import junit.framework.JUnit4TestAdapter;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
 import calculadora.TestCalculadora;
-import org.junit.jupiter.api.Test;
 
 @RunWith(Suite.class)
 @SuiteClasses({TestCalculadora.class})
 public class TesteSuite {
 
-    public static junit framework
-
-    .Test suite() {
+    public static junit framework.Test suite() {
         return new JUnit4TestAdapter(TesteSuite.class);
     }
 }
